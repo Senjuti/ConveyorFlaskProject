e929711436870eb4ded3f361edd4b4a50d4e52e4
senjuti <senjutikundu93@gmail.com>
2024-04-29 16:31:07 -0400

    Adding default redirect route

--- conveyor/flask_app.py
+++ conveyor/flask_app.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, redirect
 from flask_restx import Api
 
 from conveyor.routes.aws import aws_api
@@ -13,6 +13,7 @@ logging.basicConfig(
     level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s'
 )
 
+vanilla_blueprint = Blueprint('Vanilla Blueprint', __name__)
 blueprint = Blueprint('Conveyor API', __name__, url_prefix='/api/v1')
 api = Api(blueprint, title='Conveyor API', description='CRUD API')
 api.add_namespace(aws_api)
@@ -22,5 +23,12 @@ api.add_namespace(status_api)
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(blueprint)
+    app.register_blueprint(vanilla_blueprint)
     log.info(f'URL Map: {app.url_map}')
     return app
+
+
+@vanilla_blueprint.route('/', methods=['GET'])
+def api_root_redirect():
+    """Redirect root / to /api"""
+    return redirect('/api/v1')
